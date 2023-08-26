c075251f9d14f24124771965e606e1b6f2e33cb3
zombie-einstein <zombie-einstein@proton.me>
2023-08-26 18:19:48 +0100

    Add docstrings

--- jpviz/dot/graph_utils.py
+++ jpviz/dot/graph_utils.py
@@ -12,6 +12,25 @@ def get_arg_node(
     show_avals: bool,
     is_literal: bool,
 ) -> pydot.Node:
+    """
+    Return a pydot node representing a function input/argument
+
+    Parameters
+    ----------
+    arg_id: str
+        Unique ID of the node
+    var: jax._src.core.Var
+        JAX variable of literal instance
+    show_avals: bool
+        If `True` show the type in the node
+    is_literal: True
+        Should be `True` if the node is a literal (and
+        should be styled as such)
+
+    Returns
+    -------
+    pydot.Node
+    """
     style = styling.LITERAL_STYLING if is_literal else styling.IN_ARG_STYLING
     return pydot.Node(
         name=arg_id,
@@ -21,6 +40,22 @@ def get_arg_node(
 
 
 def get_var_node(var_id: str, var: jax_core.Var, show_avals: bool) -> pydot.Node:
+    """
+    Get a pydot node representing a variable internal to a function
+
+    Parameters
+    ----------
+    var_id: str
+        Unique ID of the node
+    var: jax._src.core.Var
+        JAX variable instance
+    show_avals: bool
+        If `True` show the type in the node
+
+    Returns
+    -------
+    pydot.Node
+    """
     return pydot.Node(
         name=var_id,
         label=utils.get_node_label(var, show_avals),
@@ -29,6 +64,22 @@ def get_var_node(var_id: str, var: jax_core.Var, show_avals: bool) -> pydot.Node
 
 
 def get_out_node(out_id: str, var: jax_core.Var, show_avals: bool) -> pydot.Node:
+    """
+    Get a pydot node representing the outputs of a function
+
+    Parameters
+    ----------
+    out_id: str
+        Unique ID of the node
+    var: jax._src.core.Var
+        JAX variable instance
+    show_avals: bool
+        If `True` show the type in the node
+
+    Returns
+    -------
+    pydot.Node
+    """
     return pydot.Node(
         name=out_id,
         label=utils.get_node_label(var, show_avals),
@@ -37,6 +88,20 @@ def get_out_node(out_id: str, var: jax_core.Var, show_avals: bool) -> pydot.Node
 
 
 def get_subgraph(graph_id: str, label: str) -> pydot.Subgraph:
+    """
+    Get a pydot subgraph
+
+    Parameters
+    ----------
+    graph_id: str
+        Unique ID of the subgraph
+    label: str
+        Label of the subgraph
+
+    Returns
+    -------
+    pydot.Subgraph
+    """
     return pydot.Subgraph(
         graph_id,
         label=label,
@@ -52,6 +117,30 @@ def get_arguments(
     parent_invars: typing.List[jax_core.Var],
     show_avals: bool,
 ) -> typing.Tuple[pydot.Subgraph, typing.List[pydot.Edge]]:
+    """
+    Generate a subgraph containing arguments, and edges connecting
+    it to its parent graph
+
+    Parameters
+    ----------
+    graph_id: str
+        ID of the subgraph that owns the arguments
+    parent_id: str
+        ID of the parent of the subgraph
+    graph_invars: List[jax._src.core.Var]
+        List of input variables to the subgraph
+    parent_invars: List[jax._src.core.Var]
+        List of the corresponding input variables from the parent subgraph
+    show_avals: bool
+        If `True` show the type in the node
+
+    Returns
+    -------
+    (pydot.Subgraph, typing.List[pydot.Edge])
+        Tuple containing the argument subgraph and a list of
+        edges that connect variables in the parent graph to
+        the inputs of this subgraph.
+    """
     argument_nodes = pydot.Subgraph(f"{graph_id}_args", rank="same")
     argument_edges = list()
 
@@ -81,6 +170,42 @@ def get_outputs(
     typing.List[pydot.Node],
     typing.List[pydot.Edge],
 ]:
+    """
+    Generate a subgraph containing function output nodes, and
+    edges and nodes that connect outputs to the parent graph
+
+    Parameters
+    ----------
+    graph_id: str
+        ID of the subgraph
+    parent_id: str
+        ID of the parent graph
+    graph_invars: List[jax._src.core.Var]
+        List of funtion input variables
+    graph_outvars: List[jax._src.core.Var]
+        List of output function variables
+    parent_outvars: List[jax._src.core.Var]
+        Corresponding list of variable from the parent
+        graph that are outputs from this graph
+    show_avals: bool
+        If `True` show the type in the node
+
+    Returns
+    -------
+    (
+        pydot.Subgraph,
+        typing.List[pydot.Edge],
+        typing.List[pydot.Node],
+        typing.List[pydot.Edge]
+    )
+        Tuple containing:
+            - The subgraph wrapping the output nodes
+            - A list of edges connecting to the parent graph
+            - A list of variable nodes that should be added to the
+              parent graph (as outputs from this graph)
+            - A list of edges that connect inputs directly to outputs
+              in the case an argument is returned by the function
+    """
     out_graph = pydot.Subgraph(f"{graph_id}_outs", rank="same")
     out_edges = list()
     out_nodes = list()
